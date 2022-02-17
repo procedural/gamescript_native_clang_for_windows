bf4eae19d20ebc597e94b6bed99210f8829ba0a3
procedural <iamvfx@gmail.com>
2022-02-18 01:13:02 +0300

    Update f1.bat

--- GameScriptNative/f1.bat
+++ GameScriptNative/f1.bat
@@ -1,9 +1,9 @@
 @if not exist C:\GameScriptNative\libgamescriptnative.o (
-  @clang++.exe -c -g -O2 -w -std=c++14 C:\GameScriptNative\libgamescriptnative.cpp -o C:\GameScriptNative\libgamescriptnative.o
+  @C:\GameScriptNative\clang++.exe -c -g -O2 -w -std=c++14 C:\GameScriptNative\libgamescriptnative.cpp -o C:\GameScriptNative\libgamescriptnative.o
 )
 
 @if not exist C:\GameScriptNative\libgamescriptnative.h.pch (
-  @clang++.exe -O2 -x c++-header libgamescriptnative.h -Xclang -emit-pch -o libgamescriptnative.h.pch
+  @C:\GameScriptNative\clang++.exe -O2 -x c++-header libgamescriptnative.h -Xclang -emit-pch -o libgamescriptnative.h.pch
 )
 
 @"C:\Program Files\Git\bin\bash.exe" C:\GameScriptNative\clang_autocomplete_windows.sh C:/GameScriptNative/f1.cpp -O2 -include C:/GameScriptNative/libgamescriptnative.h

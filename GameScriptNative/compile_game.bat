e6c50c168663f11289280a6073f36e1f6b4163e5
Constantine Tarasenkov <iamvfx@gmail.com>
2022-02-17 23:36:26 +0300

    Precompile libgamescriptnative.h

--- GameScriptNative/compile_game.bat
+++ GameScriptNative/compile_game.bat
@@ -4,4 +4,8 @@
   @clang++.exe -c -g -O2 -w -std=c++14 C:\GameScriptNative\libgamescriptnative.cpp -o C:\GameScriptNative\libgamescriptnative.o
 )
 
+@if not exist C:\GameScriptNative\libgamescriptnative.h.pch (
+  @clang++.exe -O2 -x c++-header libgamescriptnative.h -Xclang -emit-pch -o libgamescriptnative.h.pch
+)
+
 @C:\GameScriptNative\clang++.exe -g -O2 -w -std=c++14 -shared -include C:/GameScriptNative/libgamescriptnative.h C:/GameScriptNative/game.cpp C:/GameScriptNative/gamescript.dll.lib C:/GameScriptNative/libgamescriptnative.o -o C:/GameScriptNative/game.dll

58e7bef1300c5e32a208d9d84e7fc261cd7a1345
procedural <iamvfx@gmail.com>
2022-02-19 11:32:43 -0800

    Update compile_thread2.bat

--- GameScriptNative/compile_thread2.bat
+++ GameScriptNative/compile_thread2.bat
@@ -1,11 +1,11 @@
 @del C:\GameScriptNative\thread2.dll
 
 @if not exist C:\GameScriptNative\libgamescriptnative.o (
-  @C:\GameScriptNative\clang++.exe -c -g -O2 -w -std=c++14 C:\GameScriptNative\libgamescriptnative.cpp -o C:\GameScriptNative\libgamescriptnative.o
+  @C:\GameScriptNative\clang++.exe -c -g -O1 -w -std=c++14 C:\GameScriptNative\libgamescriptnative.cpp -o C:\GameScriptNative\libgamescriptnative.o
 )
 
 @if not exist C:\GameScriptNative\libgamescriptnative.h.pch (
-  @C:\GameScriptNative\clang++.exe -O2 -x c++-header C:/GameScriptNative/libgamescriptnative.h -Xclang -emit-pch -o C:/GameScriptNative/libgamescriptnative.h.pch
+  @C:\GameScriptNative\clang++.exe -O1 -x c++-header C:/GameScriptNative/libgamescriptnative.h -Xclang -emit-pch -o C:/GameScriptNative/libgamescriptnative.h.pch
 )
 
-@C:\GameScriptNative\clang++.exe -g -O2 -w -std=c++14 -shared -include C:/GameScriptNative/libgamescriptnative.h C:/GameScriptNative/thread2.cpp C:/GameScriptNative/gamescript.dll.lib C:/GameScriptNative/libgamescriptnative.o -o C:/GameScriptNative/thread2.dll
+@C:\GameScriptNative\clang++.exe -g -O1 -w -std=c++14 -shared -include C:/GameScriptNative/libgamescriptnative.h C:/GameScriptNative/thread2.cpp C:/GameScriptNative/gamescript.dll.lib C:/GameScriptNative/libgamescriptnative.o -o C:/GameScriptNative/thread2.dll
